3bb8fe17c9636fe929c9252cd4ed83f342d0703a
jean <jeanlds13@gmail.com>
2023-04-10 23:01:52 -0300

    adicionando as alterações da api, liberando acesso de qualquer ip conectar

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+
 require('./database');
+
+const corsOptions ={// liberando acessos de qualquer url, sem essa permissão algumas urls são bloqueadas ao tentar a conexão 
+  origin:'*', 
+  credentials:true,            
+  optionSuccessStatus:200,
+}
 
 //middlewars
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //rotas
 app.use('/salao', require('./src/routes/salao.routes'));
