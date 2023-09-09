08376c89341158671b29e58a08d85d1531a02548
jean <jeanlds13@gmail.com>
2023-09-09 00:19:56 -0300

    atualizando

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
+ 
 import thunk from 'redux-thunk';
 
 import rootReducer from './rootReducer';
